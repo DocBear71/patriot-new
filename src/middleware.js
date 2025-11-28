7f5be410aafec97b95744e0dfd063cebb28c530f
Edward McKeown <edward@doctormckeown.com>
2025-11-28 16:35:13 -0600

    fixing contact us 11-28-2025

---
+++ src/middleware.js
@@ -0,0 +1,16 @@
+// src/middleware.js
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+    const response = NextResponse.next();
+
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+
+    return response;
+}
+
+export const config = {
+    matcher: '/api/:path*',
+};
